24f7d816985a7b3cb6e69eedacaf941a81603115
AnyaGalkina <anna.blackbird1@gmail.com>
2022-06-25 23:14:27 +0700

    Add store

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,4 +1,4 @@
-let renderEntireTree = () => {};
+// let renderEntireTree = () => {};
 
 export type PostType = {
     id: number;
@@ -31,55 +31,128 @@ export type StateType = {
     profilePage: ProfilePageType;
 }
 
-export const state: StateType = {
-    dialogsPage: {
-        messages: [
-            {id: 1, messageBody: 'Hi! Have a good day!'},
-            {id: 2, messageBody: 'Good luck!'},
-            {id: 3, messageBody: 'Good luck!'},
-            {id: 4, messageBody: 'Be happy!'}
-        ],
-        dialogItems: [
-            {id: 1, name: 'Andrey'},
-            {id: 2, name: 'Lena'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Olga'},
-        ],
-        newMessageText: '',
+export type StoreType = {
+    _state: StateType;
+    _renderEntireTree: (state: StateType) => void;
+    subscribe: (observer: () => void) => void;
+    addPost: () => void;
+    addMessage: () => void;
+    updateNewPostText: (newPostText: string) => void;
+    updateNewMessageText: (newMessageText: string) => void;
+    getState: () => StateType;
+}
+
+export let store: StoreType = {
+    _state: {
+        dialogsPage: {
+            messages: [
+                {id: 1, messageBody: 'Hi! Have a good day!'},
+                {id: 2, messageBody: 'Good luck!'},
+                {id: 3, messageBody: 'Good luck!'},
+                {id: 4, messageBody: 'Be happy!'}
+            ],
+            dialogItems: [
+                {id: 1, name: 'Andrey'},
+                {id: 2, name: 'Lena'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Olga'},
+            ],
+            newMessageText: '',
+        },
+        profilePage: {
+            posts: [
+                {id: 1, likesCounter: 120, postText: "Hello! Happy to see you!"},
+                {id: 2, likesCounter: 70, postText: "Good luck!"},
+            ],
+            newPostText: '',
+        }
     },
-    profilePage: {
-        posts: [
-            {id: 1, likesCounter: 120, postText: "Hello! Happy to see you!"},
-            {id: 2, likesCounter: 70, postText: "Good luck!"},
-        ],
-        newPostText: '',
+    _renderEntireTree: () => {
+    },
+
+    subscribe(observer) {
+        this._renderEntireTree = observer;
+    },
+
+    addPost() {
+        let newPost: PostType = {id: 3, likesCounter: 0, postText: this._state.profilePage.newPostText}
+        this._state.profilePage.posts.push(newPost);
+        this.updateNewPostText('');
+        // this._renderEntireTree();
+        this._renderEntireTree(this._state);
+    },
+    updateNewPostText(newPostText) {
+        this._state.profilePage.newPostText = newPostText;
+        // this._renderEntireTree();
+        this._renderEntireTree(this._state);
+    },
+    addMessage() {
+        let newMessage = {id: 10, messageBody: this._state.dialogsPage.newMessageText};
+        this._state.dialogsPage.messages.push(newMessage);
+        this.updateNewMessageText('');
+        this._renderEntireTree(this._state);
+    },
+
+    updateNewMessageText(newMessageText) {
+        this._state.dialogsPage.newMessageText = newMessageText;
+        this._renderEntireTree(this._state);
+    },
+
+    getState() {
+        return this._state
     }
-};
-
-export const subscribe = (observer: () => void)  => {
-    renderEntireTree = observer;
 }
+//window ?
 
-export const addPost = (): void => {
-    let newPost: PostType = {id: 3, likesCounter: 0, postText: state.profilePage.newPostText}
-    state.profilePage.posts.push(newPost);
-    updateNewPostText('');
-    renderEntireTree();
-}
+// export const state: StateType = {
+//     dialogsPage: {
+//         messages: [
+//             {id: 1, messageBody: 'Hi! Have a good day!'},
+//             {id: 2, messageBody: 'Good luck!'},
+//             {id: 3, messageBody: 'Good luck!'},
+//             {id: 4, messageBody: 'Be happy!'}
+//         ],
+//         dialogItems: [
+//             {id: 1, name: 'Andrey'},
+//             {id: 2, name: 'Lena'},
+//             {id: 3, name: 'Sveta'},
+//             {id: 4, name: 'Olga'},
+//         ],
+//         newMessageText: '',
+//     },
+//     profilePage: {
+//         posts: [
+//             {id: 1, likesCounter: 120, postText: "Hello! Happy to see you!"},
+//             {id: 2, likesCounter: 70, postText: "Good luck!"},
+//         ],
+//         newPostText: '',
+//     }
+// };
 
-export const updateNewPostText = (newPostText: string): void => {
-    state.profilePage.newPostText = newPostText;
-    renderEntireTree();
-}
-
-export const addMessage = () => {
-    let newMessage = {id: 10, messageBody: state.dialogsPage.newMessageText};
-    state.dialogsPage.messages.push(newMessage);
-    updateNewMessageText('');
-    renderEntireTree();
-}
-
-export const updateNewMessageText = (newMessageText: string): void => {
-    state.dialogsPage.newMessageText = newMessageText;
-    renderEntireTree();
-}
+// export const subscribe = (observer: () => void)  => {
+//     renderEntireTree = observer;
+// }
+//
+// export const addPost = (): void => {
+//     let newPost: PostType = {id: 3, likesCounter: 0, postText: state.profilePage.newPostText}
+//     state.profilePage.posts.push(newPost);
+//     updateNewPostText('');
+//     renderEntireTree();
+// }
+//
+// export const updateNewPostText = (newPostText: string): void => {
+//     state.profilePage.newPostText = newPostText;
+//     renderEntireTree();
+// }
+//
+// export const addMessage = () => {
+//     let newMessage = {id: 10, messageBody: state.dialogsPage.newMessageText};
+//     state.dialogsPage.messages.push(newMessage);
+//     updateNewMessageText('');
+//     renderEntireTree();
+// }
+//
+// export const updateNewMessageText = (newMessageText: string): void => {
+//     state.dialogsPage.newMessageText = newMessageText;
+//     renderEntireTree();
+// }
