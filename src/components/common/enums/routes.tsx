bd4f2e59f3d4c3d03047e5c99e3a35eae4cfc426
AnyaGalkina <anna.blackbird1@gmail.com>
2022-10-24 16:42:06 +0300

    Add enum for routes

---
+++ src/components/common/enums/routes.tsx
@@ -0,0 +1,10 @@
+export enum ROUTES {
+    DIALOGS = "/dialogs",
+    PROFILE = "/profile/:userId?",
+    NEWS = "/news",
+    MUSIC = "/music",
+    SETTINGS = "/settings",
+    USERS = "/developers",
+    LOGIN = "/login",
+    PAGE_NOT_FOUND = "/404"
+}
